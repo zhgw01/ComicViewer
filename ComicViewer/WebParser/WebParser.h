e28c314557e517b6f56a7fd62d03ef86239208ec
Gongwei Zhang <zhgw01@gmail.com>
2012-03-27 21:43:18 +0800

    Refactor KangDmParser

---
+++ ComicViewer/WebParser/WebParser.h
@@ -0,0 +1,13 @@
+//
+//  WebParser.h
+//  ComicViewer
+//
+//  Created by Gongwei Zhang on 3/27/12.
+//  Copyright (c) 2012 autodesk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebParser : NSObject
+
+@end
---
+++ ComicViewer/WebParser/WebParser.m
@@ -0,0 +1,13 @@
+//
+//  WebParser.m
+//  ComicViewer
+//
+//  Created by Gongwei Zhang on 3/27/12.
+//  Copyright (c) 2012 autodesk. All rights reserved.
+//
+
+#import "WebParser.h"
+
+@implementation WebParser
+
+@end
